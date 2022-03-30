3014b49b63dda01e4aa3bf3d35d3fe9675a64b26
Muhammad-Jibril B.A <jbashir52@gmail.com>
2022-03-30 12:31:17 +0800

    Add benchmarking

--- s-node/runtime/src/benchmarking/mod.rs
+++ s-node/runtime/src/benchmarking/mod.rs
@@ -25,27 +25,28 @@ use sp_runtime::traits::AccountIdConversion;
 pub mod utils;
 
 // module benchmarking
-pub mod auction_manager;
-pub mod cdp_engine;
-pub mod cdp_treasury;
-pub mod currencies;
-pub mod dex;
+pub mod launchpad_crowdsales;
+// pub mod auction_manager;
+// pub mod cdp_engine;
+// pub mod cdp_treasury;
+// pub mod currencies;
+// pub mod dex;
 // pub mod dex_oracle;
 // pub mod emergency_shutdown;
 // pub mod evm;
-pub mod evm_accounts;
-pub mod serp_setmint;
-pub mod serp_treasury;
-pub mod prices;
-pub mod transaction_pause;
-pub mod transaction_payment;
-pub mod vesting;
+// pub mod evm_accounts;
+// pub mod serp_setmint;
+// pub mod serp_treasury;
+// pub mod prices;
+// pub mod transaction_pause;
+// pub mod transaction_payment;
+// pub mod vesting;
 
 // orml benchmarking
-pub mod auction;
-pub mod authority;
-pub mod oracle;
-pub mod tokens;
+// pub mod auction;
+// pub mod authority;
+// pub mod oracle;
+// pub mod tokens;
 
 pub fn get_vesting_account() -> super::AccountId {
 	super::TreasuryPalletId::get().into_account()
